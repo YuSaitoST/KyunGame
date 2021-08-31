93f9bcc52b2b60e0ecee5ad717863d3631a5a524
MSI\STzin <s.t.zint@gmail.com>
2021-08-31 13:51:05 +0900

    再生成データ

---
+++ Hert for we/DirectX12 MainProject/MainScene.cpp
@@ -0,0 +1,136 @@
+//
+// MainScene.cpp
+//
+
+#include "Base/pch.h"
+#include "Base/dxtk.h"
+#include "SceneFactory.h"
+
+// Initialize member variables.
+MainScene::MainScene()
+{
+
+}
+
+// Initialize a variable and audio resources.
+void MainScene::Initialize()
+{
+	font = DX9::SpriteFont::CreateFromFontName(DXTK->Device9, L"FixedSys 標準", 20);
+}
+
+// Allocate all memory the Direct3D and Direct2D resources.
+void MainScene::LoadAssets()
+{
+	descriptorHeap = DX12::CreateDescriptorHeap(DXTK->Device, 1);
+
+	ResourceUploadBatch resourceUploadBatch(DXTK->Device);
+	resourceUploadBatch.Begin();
+
+	RenderTargetState rtState(DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT_D32_FLOAT);
+	SpriteBatchPipelineStateDescription pd(rtState, &CommonStates::NonPremultiplied);
+	D3D12_VIEWPORT viewport = {
+	0.0f, 0.0f, 1280.0f, 720.0f,
+	D3D12_MIN_DEPTH, D3D12_MAX_DEPTH
+	};
+
+	spriteBatch = DX12::CreateSpriteBatch(DXTK->Device, resourceUploadBatch, pd, &viewport);
+
+	dx9GpuDescriptor = DXTK->Direct3D9->CreateShaderResourceView(descriptorHeap.get(), 0);
+
+	auto uploadResourcesFinished = resourceUploadBatch.End(DXTK->CommandQueue);
+	uploadResourcesFinished.wait();
+
+	LA_Load();
+}
+
+// Releasing resources required for termination.
+void MainScene::Terminate()
+{
+	DXTK->ResetAudioEngine();
+	DXTK->WaitForGpu();
+
+	// TODO: Add your Termination logic here.
+
+}
+
+// Direct3D resource cleanup.
+void MainScene::OnDeviceLost()
+{
+
+}
+
+// Restart any looped sounds here
+void MainScene::OnRestartSound()
+{
+
+}
+
+// Updates the scene.
+NextScene MainScene::Update(const float deltaTime)
+{
+	// If you use 'deltaTime', remove it.
+	UNREFERENCED_PARAMETER(deltaTime);
+
+	// TODO: Add your game logic here.
+
+
+
+	return NextScene::Continue;
+}
+
+// Draws the scene.
+void MainScene::Render()
+{
+	// TODO: Add your rendering code here.
+
+
+	DXTK->Direct3D9->Clear(DX9::Colors::RGBA(0, 0, 0, 255));  // 画面をクリア
+	DXTK->Direct3D9->BeginScene();  // シーンの開始を宣言
+
+	DX9::SpriteBatch->Begin();  // スプライトの描画を開始
+
+
+
+	Re_NomalDraw();
+
+	DX9::SpriteBatch->End();  // スプライトの描画を終了
+	DXTK->Direct3D9->EndScene();  // シーンの終了を宣言
+
+	Re_DirectTwelve();
+}
+
+void MainScene::LA_Load() {
+	bg_1 = DX9::Sprite::CreateFromFile(DXTK->Device9, L"sp1.png");
+	bg_2 = DX9::Sprite::CreateFromFile(DXTK->Device9, L"sp2.png");
+}
+
+void MainScene::Re_NomalDraw() {
+	DX9::SpriteBatch->DrawSimple(
+		bg_1.Get(), SimpleMath::Vector3(0.0f, 0.0f, 0.0f)
+	);
+	DX9::SpriteBatch->DrawSimple(
+		bg_2.Get(), SimpleMath::Vector3(1920.0f, 0.0f, 0.0f)
+	);
+}
+
+void MainScene::Re_DirectTwelve() {
+	DXTK->Direct3D9->UpdateResource();
+
+	DXTK->ResetCommand();
+	DXTK->ClearRenderTarget(Colors::CornflowerBlue);
+
+
+	const auto heapes = descriptorHeap->Heap();
+	DXTK->CommandList->SetDescriptorHeaps(1, &heapes);
+
+	spriteBatch->Begin(DXTK->CommandList);
+	spriteBatch->Draw(
+		dx9GpuDescriptor,
+		XMUINT2(1280, 720),   // HD
+		SimpleMath::Vector2(0.0f, 0.0f)
+	);
+	spriteBatch->End();
+
+	DXTK->Direct3D9->WaitUpdate();
+	DXTK->ExecuteCommandList();
+}
---
+++ Hert for we/DirectX12 MainProject/MainScene.h
@@ -0,0 +1,50 @@
+//
+// MainScene.h
+//
+
+#pragma once
+
+#include "Scene.h"
+
+using Microsoft::WRL::ComPtr;
+using std::unique_ptr;
+using std::make_unique;
+using namespace DirectX;
+
+class MainScene final : public Scene {
+public:
+	MainScene();
+	virtual ~MainScene() { Terminate(); }
+
+	MainScene(MainScene&&) = default;
+	MainScene& operator= (MainScene&&) = default;
+
+	MainScene(MainScene const&) = delete;
+	MainScene& operator= (MainScene const&) = delete;
+
+	// These are the functions you will implement.
+	void Initialize() override;
+	void LoadAssets() override;
+
+	void Terminate() override;
+
+	void OnDeviceLost() override;
+	void OnRestartSound() override;
+
+	NextScene Update(const float deltaTime) override;
+	void Render() override;
+
+private:
+	DX12::DESCRIPTORHEAP descriptorHeap;
+	DX12::SPRITEBATCH spriteBatch;
+	DX12::HGPUDESCRIPTOR dx9GpuDescriptor;
+
+	DX9::SPRITEFONT font;
+
+	DX9::SPRITE bg_1;
+	DX9::SPRITE bg_2;
+
+	void LA_Load();
+	void Re_NomalDraw();
+	void Re_DirectTwelve();
+};
